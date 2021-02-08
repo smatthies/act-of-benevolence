2488a077dd733da3ea3241feb6f5126c4078cbcf
sarahapplied <sarah.matthies@zeb-applied.com>
2021-02-08 09:05:46 +0100

    added tshirts to overview

--- src/Components/Overview/Overview.js
+++ src/Components/Overview/Overview.js
@@ -1,22 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Overview.scss";
 import content from "../../Content/content.json";
 import { useIntl } from "react-intl";
 import { Link } from "react-router-dom";
 
-const Overview = (props) => {
+function Overview(props) {
+  const { elements } = props;
+
+  const [overviewElements, setOverviewElements] = useState([]);
+
   const intl = useIntl();
-  const elements = props.elements;
-  const overviewElements = content[elements].map((element) => {
-    return (
-      <Link to={`/${elements}/${element.id}`}>
-        <div className="element" key={element.id}>
-          {intl.formatMessage({ id: `${elements}.${element.id}.text` })}
-        </div>
-      </Link>
-    );
-  });
-  return <div className="overview">{overviewElements}</div>;
-};
+
+  useEffect(() => {
+    const loadedOverviewElements = [];
+    const imgStyles = {
+      height: "inherit",
+    };
+    const images = require.context("../../assets/content", true);
+    content[elements].forEach((element) => {
+      const mainImage = images(`./${element.mainImage}`);
+
+      loadedOverviewElements.push(
+        <Link to={`/${elements}/${element.id}`} key={element.id}>
+          <div className="element" key={element.id}>
+            <img src={mainImage.default} alt={mainImage} style={imgStyles} />
+          </div>
+        </Link>
+      );
+      setOverviewElements(loadedOverviewElements.slice());
+    });
+  }, [elements, intl]);
+
+  return (
+    <div className="overview">
+      {overviewElements.map((number) => (
+        <li className="list">{number}</li>
+      ))}
+    </div>
+  );
+}
 
 export default Overview;
