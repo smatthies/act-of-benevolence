0896b5c9932ce3d9fffb309e754a577588341cfe
sarahapplied <sarah.matthies@zeb-applied.com>
2021-03-24 22:57:25 +0100

    adjusted partners text

--- src/Sides/Partners/Partners.js
+++ src/Sides/Partners/Partners.js
@@ -3,6 +3,7 @@ import { useIntl } from "react-intl";
 import "./Partners.scss";
 const Partners = () => {
   const intl = useIntl();
+
   return (
     <div className="partners">
       <p>{intl.formatMessage({ id: "partners.text" })}</p>
