339981fc000f5533f398781201f4d26d54704c7b
sarahapplied <sarah.matthies@zeb-applied.com>
2021-04-17 17:20:42 +0200

    adjustments

--- src/Sides/Partners/Partners.js
+++ src/Sides/Partners/Partners.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { useIntl } from "react-intl";
 import "./Partners.scss";
+import clsx from "clsx";
+
 const Partners = () => {
   const intl = useIntl();
 
   return (
-    <div className="partners">
+    <div className={clsx("partners", intl.locale === "ar" && "alignTextRight")}>
       <p>{intl.formatMessage({ id: "partners.text" })}</p>
     </div>
   );
